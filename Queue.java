cb2dade68ad55af00822708cee8cf6ac7f7aa16b
prathamesh-patil-5090 <122prathamesh6002@sjcem.edu.in>
2025-06-18 07:21:04 +0530

    Done with Queue without using Front

---
+++ Queue.java
@@ -0,0 +1,77 @@
+//Queue without using Front
+public class Queue {
+    private int[] data;
+
+    private static final int DEFAULT_SIZE = 10;
+
+    int end = 0;
+
+    public Queue() {
+        this(DEFAULT_SIZE);
+    }
+
+    private Queue(int size) {
+        this.data = new int[size];
+    }
+
+    public boolean isFull() {
+        return end == data.length;
+    }
+
+    public boolean isEmpty() {
+        return end == 0;
+    }
+
+    public boolean insert(int item) {
+        if (isFull()) {
+            return false;
+        }
+        data[end++] = item;
+        return true;
+    }
+
+    public int remove() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Queue is Empty!");
+        }
+        int removed = data[0];
+        for (int i = 1; i < end; i++) {
+            data[i - 1] = data[i];
+        }
+        end--;
+        return removed;
+    }
+
+    public int front() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Queue is Empty!");
+        }
+        return data[0];
+    }
+
+    public void display() {
+        for (int i = 0; i < end; i++) {
+            System.out.print(data[i] + "<- ");
+        }
+        System.out.print("END");
+    }
+    public static void main(String[] args) throws Exception {
+        Queue queue = new Queue();
+        queue.insert(3);
+        queue.insert(6);
+        queue.insert(5);
+        queue.insert(19);
+        queue.insert(1);
+
+        queue.display();
+
+        System.out.println(queue.remove());
+        queue.insert(133);
+        queue.display();
+
+        System.out.println(queue.remove());
+        queue.insert(99);
+        queue.display();
+
+    }
+}
